92995a07da376531999581ac8618e8d946ababa2
Douglas Luman <dluman@douglasjluman.com>
2020-01-30 11:22:10 -0500

    Initial commit

---
+++ src/main/java/labthree/DisplayPotion.java
@@ -0,0 +1,30 @@
+package labthree;
+
+/** Renders one of G. Wiz's potions in a digital display.
+ *
+ * @author {YOUR NAME HERE}
+ */
+public class DisplayPotion {
+  
+  /** Entry point.
+   *
+   * @param args The command line arguments
+   */
+  public static void main(String[] args) {
+    // TODO: Add comments to label the areas of your program
+    String title // TODO: Add potion title
+    String binding // TODO: Assign binding ingredient name
+    String liquid // TODO: Assign liquid ingredient name
+    String material //TODO: Assign material ingredient name
+
+    int yieldAmt = 1;
+    int bindingAmt //TODO: Assign binding ingredient amount
+    int liquidAmt //TODO: Assign liquid ingredient amount
+    int materialAmt //TODO: Assign material ingredient amount
+	
+    /*
+	 * TODO: Create print and prinln statements to accurately display
+	 *       the recipe card in the example format
+	 */
+  }
+}
